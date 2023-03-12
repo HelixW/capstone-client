64cfc41b1dfa9b6cd4462324048ec6856cf4dfd7
Shreyas K <shreyas.2000@hotmail.com>
2023-03-12 22:29:38 +0530

    feat: add upload button
    Signed-off-by: Shreyas K <shreyas.2000@hotmail.com>

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  title = 'Dashboard';
+
+  constructor(private http: HttpClient) {}
+
+  file: File | undefined;
+  fileName = '';
+
+  @ViewChild('fileUpload', { static: false })
+  fileInput: ElementRef | undefined;
+
+  removeFile() {
+    if (this.fileInput) this.fileInput.nativeElement.value = '';
+    this.file = undefined;
+    this.fileName = '';
+    console.log(this.file);
+  }
+
+  getFile(event: any) {
+    this.file = event.target.files[0];
+    if (this.file) this.fileName = this.file.name;
+    console.log(this.file);
+  }
+}
