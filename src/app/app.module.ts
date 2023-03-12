6e469328986ece47088e33327e55507dc567e683
Shreyas K <shreyas.2000@hotmail.com>
2023-03-12 21:32:32 +0530

    feat:add dashboard
    Signed-off-by: Shreyas K <shreyas.2000@hotmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'app', component: DashboardComponent },
   { path: '**', redirectTo: '' },
 ];
 
@@ -20,8 +24,14 @@ const routes: Routes = [
     HomeComponent,
     LoginComponent,
     RegisterComponent,
+    DashboardComponent,
   ],
-  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+    FormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
