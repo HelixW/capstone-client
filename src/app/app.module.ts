5485f93f1ee1c7ce1565bbda0a1250467218235d
Shreyas K <shreyas.2000@hotmail.com>
2023-03-12 18:57:16 +0530

    feat: add login component
    Signed-off-by: Shreyas K <shreyas.2000@hotmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,23 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+  ],
   imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
