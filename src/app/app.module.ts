63711477d5f00e2958159e22d7ec18e412a01260
Shreyas K <shreyas.2000@hotmail.com>
2023-05-07 13:06:12 +0530

    add: init browse page
    Signed-off-by: Shreyas K <shreyas.2000@hotmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'app', component: DashboardComponent },
+  { path: 'browse', component: BrowseComponent },
   { path: '**', redirectTo: '' },
 ];
 
--- src/app/components/browse/browse.component.ts
+++ src/app/components/browse/browse.component.ts
@@ -1,10 +1,37 @@
+import { HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-browse',
   templateUrl: './browse.component.html',
-  styleUrls: ['./browse.component.scss']
+  styleUrls: ['./browse.component.scss'],
 })
 export class BrowseComponent {
+  constructor(private user: UserService, private router: Router) {}
 
+  ngOnInit() {
+    const bearerToken = localStorage.getItem('tk');
+
+    // Check validity of token
+    if (bearerToken) {
+      var header = {
+        headers: new HttpHeaders().set(
+          'Authorization',
+          `Bearer ${bearerToken}`
+        ),
+      };
+
+      this.user.validateUser(header).subscribe({
+        next: () => console.log('User validated.'),
+        error: () => this.router.navigateByUrl(''),
+      });
+    } else this.router.navigateByUrl('');
+  }
+
+  logout() {
+    localStorage.removeItem('tk');
+    this.router.navigateByUrl('');
+  }
 }
