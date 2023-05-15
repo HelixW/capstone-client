6bcc3a73402c4843cff8cb25e8dfb29853f9319d
Shreyas K <shreyas.2000@hotmail.com>
2023-05-15 21:00:09 +0530

    fix: update browse page with human readable file size
    Signed-off-by: Shreyas K <shreyas.2000@hotmail.com>

--- src/app/components/browse/browse.component.ts
+++ src/app/components/browse/browse.component.ts
@@ -59,6 +59,16 @@ export class BrowseComponent {
     console.log('Hash copied.');
   }
 
+  humanFileSize(bytes: number) {
+    if (bytes == 0) {
+      return '0.00 B';
+    }
+    var e = Math.floor(Math.log(bytes) / Math.log(1024));
+    return (
+      (bytes / Math.pow(1024, e)).toFixed(2) + ' ' + ' KMGTP'.charAt(e) + 'B'
+    );
+  }
+
   async onDownload(hash: string) {
     this.ipfs.downloadFile(hash).subscribe({
       next: (res: any) => {
